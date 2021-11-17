2667104e6403c77293aab2074100afc32a70ad80
bmac01 <92854061+bmac01@users.noreply.github.com>
2021-11-17 17:38:59 +0000

    update of HTML and js files

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const resultDisplay = document.getElementById("result")
-const computerPickDisplay = document.getElementById("computerPick")
-const userPickDisplay = document.getElementById("userPick")
-const resultDisplay = document.getElementById('result')
+const resultDisplay = document.getElementById("result");
+const computerPickDisplay = document.getElementById("computer-pick");
+const userPickDisplay = document.getElementById("user-pick");
+const possibleChoices = document.querySelectorAll('button')
+let userPick
+let computerPick
+let result
 
-
-//fuction to take user pick from buttons 
-
-
-
-
-
+//fuction to take user pick from buttons using event listener and run computerPick and playGame fuctions
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+	userPick = e.target.id
+	userPickDisplay.innerHTML = userPick;
+	generateComputerPick()
+	playGame()
+}))
 
 //fuction to generate computer pick
-function computerPick (){
+function generateComputerPick(){
 	let computerPick=Math.random()*100;
 		if (computerPick <33) {
 		computerPick ="rock";
@@ -25,31 +28,32 @@ function computerPick (){
 }
 
 //set-up function for game, which contains the if logic for each choice the user can make
-function playGame (){
+function playGame(){
 //code for rock choice
 	if (userPick == "rock" && computerPick == "rock"){
 		result = "Draw";
 	} else if (userPick == "rock" && computerPick == "paper"){
 		result = "Computer wins! paper beats rock";
 	} else if (userPick == "rock" && computerPick == "scissors"){
-		result = "Player wins! rock beats scissors;
+		result = "Player wins! rock beats scissors";
 }
 
 //code for paper choice
 if (userPick == "paper" && computerPick == "rock"){
-		result "Player wins! paper beats rock";
+		result = "Player wins! paper beats rock";
 	} else if (userPick == "paper" && computerPick == "paper"){
-		result "Draw";
+		result = "Draw";
 	} else if (userPick == "paper" && computerPick == "scissors"){
-		result "Computer wins! scissors beats paper;
+		result = "Computer wins! scissors beats paper";
 }
 
 //code for scissors
 if (userPick == "scissors" && computerPick == "rock"){
-		result "Computer wins! rock beats scissors";
+		result = "Computer wins! rock beats scissors";
 	} else if (userPick == "scissors" && computerPick == "paper"){
-		result "Player wins! scissors beats paper";
+		result = "Player wins! scissors beats paper";
 	} else if (userPick == "scissors" && computerPick == "scissors"){
-		result "Draw";
-		resultDisplay.innerHTML = result
+		result = "Draw";
+	}
+		resultDisplay.innerHTML = result;
 }
